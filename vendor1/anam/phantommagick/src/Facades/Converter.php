ceea2e67c862ab6f1c0f1456474a870b520ed681
tnhung090299 <103357556+tnhung090299@users.noreply.github.com>
2022-04-13 14:10:23 +0700

    deletevendor_updatecomposer

--- vendor1/anam/phantommagick/src/Facades/Converter.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Anam\PhantomMagick\Facades;
-
-use Illuminate\Support\Facades\Facade;
-
-class Converter extends Facade
-{
-    protected static function getFacadeAccessor() { return 'converter'; }
-}
